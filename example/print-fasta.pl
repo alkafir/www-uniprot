c158b9ec76f723bad6d980c0bf529b175cf60fde
Alfredo Mungo <alfredo.mungo@openmailbox.org>
2015-06-07 16:21:24 +0200

    Added example

---
+++ example/print-fasta.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use warnings;
+
+BEGIN { # Use bundled library
+  require File::Basename;
+  require Cwd;
+
+  unshift @INC, Cwd::abs_path((File::Basename::fileparse(__FILE__))[1] . '../lib');
+}
+
+use WWW::UniProt;
+
+print WWW::UniProt::get_protein 'Q9NII1'; # Print FASTA
